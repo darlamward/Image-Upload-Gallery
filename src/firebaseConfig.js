87326c1c75395d72d7ae6d46b7a6640e5d2b08a2
darlamward <darlamward@gmail.com>
2024-09-16 14:52:47 -0230

    Added .env file to project.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,14 +4,22 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD1bhPomIU-GzKARkEpdm_8fC6_IwutSVc",
-    authDomain: "parados-60cae.firebaseapp.com",
-    projectId: "parados-60cae",
-    storageBucket: "parados-60cae.appspot.com",
-    messagingSenderId: "924519874470",
-    appId: "1:924519874470:web:eaaafa29c85ac3b723d967",
-    measurementId: "G-BME52XM5XL"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
+
+  console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+  console.log(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
+  console.log(process.env.REACT_APP_FIREBASE_PROJECT_ID);
+  console.log(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
+  console.log(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID);
+  console.log(process.env.REACT_APP_FIREBASE_APP_ID);
+  console.log(process.env.REACT_APP_FIREBASE_MEASUREMENT_ID);
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
