168d6be401c56656e9b4c0daed56c9650b77afca
darlamward <darlamward@gmail.com>
2024-09-16 16:48:36 -0230

    Creating new firebase project with different credentials.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -13,14 +13,6 @@ const firebaseConfig = {
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
 
-  console.log(process.env.REACT_APP_FIREBASE_API_KEY);
-  console.log(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
-  console.log(process.env.REACT_APP_FIREBASE_PROJECT_ID);
-  console.log(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
-  console.log(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID);
-  console.log(process.env.REACT_APP_FIREBASE_APP_ID);
-  console.log(process.env.REACT_APP_FIREBASE_MEASUREMENT_ID);
-
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 const firestore = getFirestore(app);
